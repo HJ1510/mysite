73af5f6594d586abb6593fedeb22c82b437fef65
HJ1510 <yhj221004@gmail.com>
2022-11-09 13:56:10 +0900

    mysite02

--- mysite02/src/main/java/com/bitacademy/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bitacademy.mysite.dao.UserDao;
+import com.bitacademy.mysite.vo.UserVo;
+
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -17,9 +20,52 @@ public class UserController extends HttpServlet {
 
 		String action = request.getParameter("a");
 		if ("joinform".equals(action)) {
-			request.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp").forward(request, response);
+			request
+			.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
+			.forward(request, response);
 		} else if ("join".equals(action)) {
+			String name= request.getParameter("name");
+			String email= request.getParameter("email");
+			String password= request.getParameter("password");
+			String gender= request.getParameter("gender");
 			
+			UserVo vo= new UserVo();
+			vo.setName(name);
+			vo.setEmail(email);
+			vo.setPassword(password);
+			vo.setGender(gender);
+						
+			new UserDao().insert(vo);
+			
+			response.sendRedirect(request.getContextPath() + "/user?a=joinsuccess");
+			
+	    } else if ("joinsuccess".equals(action)) {
+			request
+			.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp")
+			.forward(request, response);
+		} else if ("loginform".equals(action)) {
+			request
+			.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp")
+		    .forward(request, response);
+		}else if ("login".equals(action)) {
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			
+			UserVo authUser = new UserDao().findByEmailAndPassword(email, password);
+			
+			if(authUser==null) {
+			/* 인증실패 */
+				request.setAttribute("email", email);
+				request
+				.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp")
+			    .forward(request, response);
+				return;
+			}
+			
+			/* 로그인처리 */
+			
+			response.sendRedirect(request.getContextPath());
+					
 		}
 	}
 
