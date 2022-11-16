7743917712a90d0cfaa8e39f86c287faf626a039
HJ1510 <yhj221004@gmail.com>
2022-11-16 16:47:18 +0900

    mysite03 guestbook

---
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/GuestbookController.java
@@ -0,0 +1,47 @@
+package com.bitacademy.mysite.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bitacademy.mysite.service.GuestbookService;
+import com.bitacademy.mysite.vo.GuestbookVo;
+
+@Controller
+@RequestMapping("/guestbook")
+public class GuestbookController {
+
+	@Autowired
+	private GuestbookService guestbookService;
+	
+	@RequestMapping({ "/list", "" })
+	public String list(Model model) {
+		model.addAttribute("list", guestbookService.getContents());
+		return "guestbook/list";
+	}
+	
+	@RequestMapping("/add")
+	public String add(GuestbookVo vo) {
+		guestbookService.addContents(vo);
+		return "redirect:/guestbook";
+	}
+	
+	@RequestMapping(value = "/delete/{no}", method = RequestMethod.GET)
+	public String delete(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no", no);
+		return "guestbook/delete";
+	}
+	
+	@RequestMapping(value = "/delete/{no}", method = RequestMethod.POST)
+	public String delete(
+		@PathVariable("no") Long no,
+		@RequestParam(value="password", required=true, defaultValue="") String password) {
+//		System.out.println(no+"|"+password); 
+		guestbookService.deleteContents(no, password);
+		return "redirect:/guestbook";
+	}	
+}
