c7a77a5a2f80b50c99d23aa4f9a08ee0799b2ebb
HJ1510 <yhj221004@gmail.com>
2022-11-17 14:57:29 +0900

    mybatis

--- mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
@@ -80,7 +80,6 @@ public class GuestbookRepository {
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getPassword());
 			pstmt.setString(3, vo.getContents());
-			//
 
 			int count = pstmt.executeUpdate(); // int count = pstmt.executeUpdate(sql); 에러!
 
