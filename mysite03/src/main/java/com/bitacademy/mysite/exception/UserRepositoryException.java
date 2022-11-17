1aeebb031c3bbfc9b6b9fe1934dc04c14d9266a2
HJ1510 <yhj221004@gmail.com>
2022-11-17 12:24:07 +0900

    exception

--- mysite03/src/main/java/com/bitacademy/mysite/exception/UserRepositoryException.java
+++ mysite03/src/main/java/com/bitacademy/mysite/exception/UserRepositoryException.java
@@ -5,7 +5,7 @@ public class UserRepositoryException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public UserRepositoryException() {
-		super("UserDaoException Occurs");
+		super("UserRepositoryException Occurs");
 	}
 	
 	public UserRepositoryException(String message) {
