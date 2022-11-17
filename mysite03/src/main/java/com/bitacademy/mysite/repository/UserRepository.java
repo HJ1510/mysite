1df3f19042c00c3dda9a3a909d579dc0373cee35
HJ1510 <yhj221004@gmail.com>
2022-11-17 11:09:44 +0900

    exception

--- mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 
 import org.springframework.stereotype.Repository;
 
+import com.bitacademy.mysite.exception.UserRepositoryException;
 import com.bitacademy.mysite.vo.UserVo;
 
 @Repository
@@ -113,7 +114,7 @@ public class UserRepository {
 		return result;
 	}
 
-	public UserVo findByEmailAndPassword(String email, String password) {
+	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException{
 		UserVo result = null;
 
 		Connection conn = null;
@@ -141,7 +142,8 @@ public class UserRepository {
 			}
 
 		} catch (SQLException e) {
-			System.out.println("Error:" + e);
+			throw new UserRepositoryException(e.toString());
+			
 		} finally {
 			try {
 				if (pstmt != null) {
