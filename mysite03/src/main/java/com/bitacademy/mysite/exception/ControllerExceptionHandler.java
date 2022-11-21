29821ba190cc373e5fbbd52d7a0d17a5dba199c0
HJ1510 <yhj221004@gmail.com>
2022-11-21 11:52:43 +0900

    logging

--- mysite03/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
+++ mysite03/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package com.bitacademy.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice //controller에 AOP 적용 // spring-servlet.xml에 경로 설정 추가해야함
 public class ControllerExceptionHandler {
+	
+	private static final Log Logger = LogFactory.getLog(ControllerExceptionHandler.class);
 
 	@ExceptionHandler(Exception.class)
 	public String HandlerException(Model model, Exception e) { 
@@ -17,6 +21,8 @@ public class ControllerExceptionHandler {
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		System.out.println(errors.toString()); // 실제로는 화면출력x 로그파일을 저장해야함
+		Logger.error(errors.toString());
+		
 		
 		// 사과페이지(HTML 응답, 정상종료)
 		model.addAttribute("exception", errors.toString());
