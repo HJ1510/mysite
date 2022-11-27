ce68453e32a28e4add3b2b23c9426f756e829c0b
user <user@HOME>
2022-11-28 01:39:35 +0900

    mysite04

--- mysite04/src/main/java/com/bitacademy/mysite/controller/BoardController.java
+++ mysite04/src/main/java/com/bitacademy/mysite/controller/BoardController.java
@@ -1,15 +1,20 @@
 package com.bitacademy.mysite.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bitacademy.mysite.security.Auth;
+import com.bitacademy.mysite.security.AuthUser;
 import com.bitacademy.mysite.service.BoardService;
 import com.bitacademy.mysite.vo.BoardVo;
+import com.bitacademy.mysite.vo.UserVo;
 
 @Controller
 @RequestMapping("/board")
@@ -18,22 +23,38 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
-	@RequestMapping({"/list", ""}) // 임시완료
-	public String list(Model model) {
-		model.addAttribute("list", boardService.getContents());
-		return "board/list";		
+	@RequestMapping({"/index", ""}) // 임시완료
+//	public String list(Model model) {
+//		model.addAttribute("list", boardService.getContentsList());
+//		return "board/list";		
+//	}
+	
+	public String index(@RequestParam(value="p", required=true, defaultValue="1") Integer page, @RequestParam(value="kwd", required=true, defaultValue="") String keyword, Model model){
+		Map<String, Object> map = boardService.getContentsList(page, keyword);
+		model.addAttribute("map", map);			
+		return "board/index";
 	}
 	
-//	public Map<String, Object> findContentsList(int currentPage){
-//		// 리스트 가져오기
-//		
-//		// view의 페이징을 처리하기 위한 데이터의 값 계산
-//		int beginPage=0;
-//		int endPage=0;
-//		
-//		return null;
+	@RequestMapping("/view/{no}") // 완
+	public String view(@PathVariable("no") Long no, Model model) {
+		BoardVo boardVo = boardService.getContents(no);
+		model.addAttribute("boardVo", boardVo);	
+		return "board/view";
+	}
+
+	
+	@Auth
+	@RequestMapping({"/delete/{no}"})
+//	public String delete(@PathVariable("no") Long no) {
+//		boardService.deleteContents(no);
+//		return "redirect:/board";
 //	}
-//	
+	
+	public String delete(@AuthUser UserVo authUser, @PathVariable("no") Long boardNo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
+			boardService.deleteContents(boardNo, authUser.getNo());		
+		return "redirect:/board?p=" + page + "&kwd=";
+	}
+	
 	@Auth
 	@RequestMapping(value = {"/write/{no}", "/write"}, method = RequestMethod.GET) // 완료
 	public String write(@PathVariable("no") Long no, Model model) {
@@ -51,35 +72,11 @@ public class BoardController {
 		return "redirect:/board";
 	}
 	
-	@RequestMapping("/view/{no}")
-	public String view(@PathVariable("no") Long no, Model model) {
-		BoardVo boardVo = boardService.findContents(no);
-		model.addAttribute("title", boardVo.getTitle());
-		model.addAttribute("contents", boardVo.getContents());
-		model.addAttribute("userNo", boardVo.getUserNo()); 
-		boardService.hitCountUp(no);
-//		System.out.println("1:"+ model);		
-//		System.out.println("1vo:"+ boardVo);		
-		return "board/view";
-	}
-	
-	@Auth
-	@RequestMapping({"/delete/{no}/{userNo}", "/delete/{no}", "delete"})
-//	public String delete(@PathVariable("no") Long no) {
-//		boardService.deleteContents(no);
-//		return "redirect:/board";
-//	}
-	
-	public String delete(@PathVariable("no") Long no, @PathVariable(value="userNo", required=false) Long userNo) {
-			boardService.deleteContents(no, userNo);		
-		return "redirect:/board";
-	}
-	
 	
 	@Auth
 	@RequestMapping(value = {"/modify/{no}", "/modify"}, method = RequestMethod.GET)
 	public String modify(@PathVariable("no") Long no, Model model) {
-		BoardVo boardVo = boardService.findContents(no);
+		BoardVo boardVo = boardService.getContents(no);
 		model.addAttribute("no", no);
 		model.addAttribute("title", boardVo.getTitle());
 		model.addAttribute("contents", boardVo.getContents());
--- mysite04/src/main/java/com/bitacademy/mysite/service/BoardService.java
+++ mysite04/src/main/java/com/bitacademy/mysite/service/BoardService.java
@@ -1,5 +1,6 @@
 package com.bitacademy.mysite.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,35 +12,68 @@ import com.bitacademy.mysite.vo.BoardVo;
 
 @Service
 public class BoardService {
-	
+	private static final int LIST_SIZE = 5; // 페이지당 게시물 수
+	private static final int PAGE_SIZE = 5; // index 하단에 표시되는 페이지 수
+		
 	@Autowired
 	private BoardRepository boardRepository;
 	
-
 	
-	public List<BoardVo> getContents(){
-		return boardRepository.findAll();
-	}
-	
-//	public Map<String, Object> findContentsList(int currentPage){
-//		// 리스트 가져오기
-//		boardRepository.findAll();
-//		// view의 페이징을 처리하기 위한 데이터의 값 계산
-//		int beginPage=0;
-//		int endPage=0;
-//		
-//		return null;
+//	public List<BoardVo> getContentsList(){
+//		return boardRepository.findAll();
 //	}
 	
-	public void addContents(BoardVo vo) {
-		boardRepository.insert(vo);
-//		System.out.println("2:"+vo);
-		//답글여부 확인해서 처리 groupNo==null
+	public Map<String, Object> getContentsList(int currentPage, String keyword){
+		
+		// view의 페이징을 처리하기 위한 데이터의 값 계산
+		// 1. 페이징을 위한 기본 데이터 계산
+		int totalCount = boardRepository.getWholeCount(keyword);
+		int pageCount = (int) Math.ceil((double) totalCount / LIST_SIZE);
+		int blockCount = (int) Math.ceil((double) pageCount / PAGE_SIZE);
+		int currentBlock = (int) Math.ceil((double) currentPage / PAGE_SIZE);
+		// 2. 파라미터 page 값 검증
+		if (currentPage > pageCount) {
+			currentPage = pageCount;
+			currentBlock = (int) Math.ceil((double) currentPage / PAGE_SIZE);
+		}
+
+		if (currentPage < 1) {
+			currentPage = 1;
+			currentBlock = 1;
+		}
+		// 3. view에서 페이지 리스트를 렌더링 하기위한 데이터 값 계산
+		int beginPage = currentBlock == 0 ? 1 : (currentBlock - 1) * PAGE_SIZE + 1;
+		int prevPage = (currentBlock > 1) ? (currentBlock - 1) * PAGE_SIZE : 0;
+		int nextPage = (currentBlock < blockCount) ? currentBlock * PAGE_SIZE + 1 : 0;
+		int endPage = (nextPage > 0) ? (beginPage - 1) + LIST_SIZE : pageCount;
+
+		// 리스트 가져오기
+		List<BoardVo> list =boardRepository.findAllByPageAndKeyword(currentPage, keyword, LIST_SIZE);
+		
+		// 리스트->맵
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("list", list);
+		map.put("currentPage", currentPage);
+		map.put("keyword", keyword);		
+		map.put("listSize", LIST_SIZE);
+		map.put("totalCount", totalCount);
+		map.put("beginPage", beginPage);
+		map.put("prevPage", prevPage);
+		map.put("nextPage", nextPage);
+		map.put("endPage", endPage);
+		
+	
+		return map;
 	}
 	
-	public BoardVo findContents(Long no) {	
-//		System.out.println("2:"+no);
-		return boardRepository.findByNo(no); //hit++
+	public BoardVo getContents(Long no) {	//view 완
+		BoardVo boardVo = boardRepository.findByNo(no);
+		
+		if(boardVo != null) {
+			boardRepository.hitCountUp(no);
+		}
+//		System.out.println("2:"+boardVo);
+		return boardVo; //hit++
 	}
 		
 	
@@ -47,21 +81,23 @@ public class BoardService {
 //		boardRepository.deleteByNo(no);
 //	}
 	
-	public void deleteContents(Long no, Long userNo) {
-		if(userNo == null) {
-			return;
-		}		
-		boardRepository.deleteByNoAndUserNo(no, userNo);
+	public void deleteContents(Long boardNo, Long userNo) {
+		boardRepository.delete(boardNo, userNo);		
 	}
+	
+	public void addContents(BoardVo vo) {
+		boardRepository.insert(vo);
+//		System.out.println("2:"+vo);
+		//답글여부 확인해서 처리 groupNo==null
+	}
+	
+
+	
+
 	
 	public void updateContents(BoardVo vo) {
 		boardRepository.update(vo);
 //		System.out.println("2:"+vo);		
-	}
-
-	public void hitCountUp(Long no) {
-		boardRepository.hitCountUp(no);
-		
 	}
 
 	public BoardVo findContentsForReply(Long no) {	
@@ -71,5 +107,7 @@ public class BoardService {
 	public void replyContents(BoardVo vo) {
 		boardRepository.replyInsert(vo);		
 	}
+
+
 
 }
