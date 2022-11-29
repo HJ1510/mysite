08dd42d7c66ae74684d827500976c692f4f4d441
HJ1510 <yhj221004@gmail.com>
2022-11-29 13:41:56 +0900

    application.yml : resource handler

--- mysite05/src/main/java/com/bitacademy/mysite/config/SecurityConfig.java
+++ mysite05/src/main/java/com/bitacademy/mysite/config/SecurityConfig.java
@@ -2,15 +2,11 @@ package com.bitacademy.mysite.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.bitacademy.mysite.security.AuthInterceptor;
@@ -19,12 +15,8 @@ import com.bitacademy.mysite.security.LoginInterceptor;
 import com.bitacademy.mysite.security.LogoutInterceptor;
 
 @SpringBootConfiguration
-@PropertySource("classpath:web/fileupload.properties") //addResourceHandlers
-public class WebConfig implements WebMvcConfigurer {
-	
-	@Autowired
-	private Environment env;
-	
+public class SecurityConfig implements WebMvcConfigurer {
+
 	// Argument Resolvers
 	@Bean
 	public HandlerMethodArgumentResolver authUserHandlerMethodArgumentResolver() {
@@ -65,13 +57,4 @@ public class WebConfig implements WebMvcConfigurer {
 			.excludePathPatterns("/user/logout")
 			.excludePathPatterns("/assets/**");
 	}
-	
-	// file upload 가상 url
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry
-			.addResourceHandler(env.getProperty("fileupload.resourceMapping")+"/**")
-			.addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation")+"/");
-	}
-
 }
